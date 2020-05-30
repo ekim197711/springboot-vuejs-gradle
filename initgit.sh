87a42c7b26cd82577f0b05f29c5d8b2476f5f0a2
Mike Møller Nielsen <mmni@protonmail.com>
2020-05-30 19:27:54 +0200

    up and running

---
+++ initgit.sh
@@ -0,0 +1,6 @@
+echo "# springboot-vuejs-gradle" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin git@github.com:ekim197711/springboot-vuejs-gradle.git
+git push -u origin master
